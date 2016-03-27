3d3284282f7a72aeb925f55494d22abe2bac82e4
P1NGA <pastor399@gmail.com>
2016-03-27 23:42:45 +1100

    Initial M67 Sight Unit Commit (Very Rough!)

---
+++ addons/arty_mortar_optics/config/CfgVehicles.hpp
@@ -0,0 +1,11 @@
+class CfgVehicles {
+	class Arty_Optic_Base;	
+	class ACE_Arty_M67: Arty_Optic_Base {
+		scope = 2;
+		scopeCurator = 2;
+		displayName = "M67 Mortar Optics";
+		author = ECSTRING(common,ACETeam);
+		model=PATHTOF(optics_m67.p3d);
+		vehicleClass = "ACE_Arty_System";
+	};	
+};
---
+++ addons/arty_mortar_optics/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT arty_mortar_optics
+
+#include "\z\ace\addons\main\script_mod.hpp"
+
+#include "\z\ace\addons\main\script_macros.hpp"
