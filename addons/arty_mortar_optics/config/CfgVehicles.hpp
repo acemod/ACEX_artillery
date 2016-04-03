cdf7b6a589f7aa0b7942e207e2e6e0fb99f128ff
P1NGA <pastor399@gmail.com>
2016-04-03 17:14:23 +1000

    M67 Optic Update

--- addons/arty_mortar_optics/config/CfgVehicles.hpp
+++ addons/arty_mortar_optics/config/CfgVehicles.hpp
@@ -1,11 +1,15 @@
 class CfgVehicles {
-	class Arty_Optic_Base;	
-	class ACE_Arty_M67: Arty_Optic_Base {
+	class ThingX;
+	class Arty_Optic_Base: ThingX {   
 		scope = 2;
-		scopeCurator = 2;
-		displayName = "M67 Mortar Optics";
+        scopeCurator = 2;
+	};
+	class ACE_Arty_M67: Arty_Optic_Base {
+		displayName = "M67 Mortar Sight";
 		author = ECSTRING(common,ACETeam);
 		model=PATHTOF(optics_m67.p3d);
 		vehicleClass = "ACE_Arty_System";
+		editorCategory = "EdCat_ACE_Arty_System";
+		editorSubcategory = "EdSubcat_ACE_Arty_Equipment";
 	};	
 };
